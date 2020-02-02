a6d59724865a2714d1a110636b5ac85295ca6c82
0yoyoyo <60439919+0yoyoyo@users.noreply.github.com>
2020-02-02 22:06:51 +0900

    Add code to create a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    println!("args: {:?}", args);
+
+    let num = &args[1];
+    println!("num: {}", num);
+
+    let mut f = File::create("num.txt")?;
+    f.write_fmt(format_args!("{}", num))?;
+    Ok(())
 }
