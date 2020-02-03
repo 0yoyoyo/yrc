143ef5e6d91ecdcca337821e13fbc0a63e703fa6
0yoyoyo <60439919+0yoyoyo@users.noreply.github.com>
2020-02-03 20:59:11 +0900

    Add creating output directory

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -7,8 +8,11 @@ fn main() -> std::io::Result<()> {
     println!("args: {:?}", args);
 
     if args.len() == 2 {
+        match fs::create_dir("output") {
+            _ => (),
+        };
         let num = &args[1];
-        let mut f = File::create("num.txt")?;
+        let mut f = File::create("output/num.txt")?;
         f.write_fmt(format_args!("{}", num))?;
     } else {
         println!("Invalid!");
