7ee19bc3f6e5c1ec7de803a04c41d2f17f625ce6
0yoyoyo <60439919+0yoyoyo@users.noreply.github.com>
2020-02-04 21:44:19 +0900

    Separate assembly generation into a function

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,41 @@ use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 
-fn main() -> std::io::Result<()> {
+fn main() {
     let args: Vec<String> = env::args().collect();
     println!("args: {:?}", args);
 
     if args.len() == 2 {
-        match fs::create_dir("output") {
-            _ => (),
+        match generate_asm(args[1].parse().unwrap()){
+            Ok(_) => (),
+            Err(_) => println!("Failed!"),
         };
-        let num = &args[1];
-        let mut f = File::create("output/tmp.s")?;
-        f.write_fmt(format_args!(".intel_syntax noprefix\n"))?;
-        f.write_fmt(format_args!(".global main\n"))?;
-        f.write_fmt(format_args!("main:\n"))?;
-        f.write_fmt(format_args!("    mov rax, {}\n", num))?;
-        f.write_fmt(format_args!("    ret\n"))?;
     } else {
         println!("Invalid!");
     }
+}
+
+fn generate_asm(num: u32) -> std::io::Result<()> {
+    match fs::create_dir("output") {
+        _ => (),
+    };
+    let mut f = File::create("output/tmp.s")?;
+    f.write_fmt(format_args!(".intel_syntax noprefix\n"))?;
+    f.write_fmt(format_args!(".global main\n"))?;
+    f.write_fmt(format_args!("main:\n"))?;
+    f.write_fmt(format_args!("    mov rax, {}\n", num))?;
+    f.write_fmt(format_args!("    ret\n"))?;
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    //use super::*;
+    use super::*;
     use std::process::Command;
 
     #[test]
     fn return_val_0() {
+        assert_eq!((), generate_asm(0).unwrap());
         let out = Command::new("bash")
                           .arg("-c")
                           .arg("script/test.sh 0 0")
