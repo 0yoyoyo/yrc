891eef5129699f49776b0c054b8d4652bbaeb2b4
0yoyoyo <60439919+0yoyoyo@users.noreply.github.com>
2020-02-06 22:59:55 +0900

    Add support for simple formulas
    It supports only numbers,  '+' and '-' which are devided by
    whitespace.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     println!("args: {:?}", args);
 
     if args.len() == 2 {
-        match generate_asm(args[1].parse().unwrap()){
+        match generate_asm(&args[1]) {
             Ok(_) => (),
             Err(_) => println!("Failed!"),
         };
@@ -17,15 +17,33 @@ fn main() {
     }
 }
 
-fn generate_asm(num: u32) -> std::io::Result<()> {
+fn generate_asm(formula: &str) -> std::io::Result<()> {
     match fs::create_dir("output") {
         _ => (),
     };
+    let mut formula = formula.split_whitespace();
     let mut f = File::create("output/tmp.s")?;
     f.write_fmt(format_args!(".intel_syntax noprefix\n"))?;
     f.write_fmt(format_args!(".global main\n"))?;
     f.write_fmt(format_args!("main:\n"))?;
-    f.write_fmt(format_args!("    mov rax, {}\n", num))?;
+    f.write_fmt(format_args!("    mov rax, {}\n",
+                             formula.next().unwrap()))?;
+
+    loop {
+        match formula.next() {
+            Some(item) => {
+                if item == "+" {
+                    f.write_fmt(format_args!("    add rax, {}\n",
+                                             formula.next().unwrap()))?;
+                } else if item == "-" {
+                    f.write_fmt(format_args!("    sub rax, {}\n",
+                                             formula.next().unwrap()))?;
+                }
+            }
+            None => break,
+        }
+    }
+
     f.write_fmt(format_args!("    ret\n"))?;
     Ok(())
 }
@@ -35,24 +53,24 @@ mod tests {
     use super::*;
     use std::process::Command;
 
-    fn return_val_num(n: u32) {
-        assert_eq!((), generate_asm(n).unwrap());
+    fn return_val_num(formula: &str) {
+        assert_eq!((), generate_asm(formula).unwrap());
         let out = Command::new("bash")
                           .arg("-c")
                           .arg("script/test.sh")
                           .output()
                           .expect("Exec error!");
-        assert_eq!(format!("{}\n", n),
+        assert_eq!(format!("{}\n", formula),
                    String::from_utf8(out.stdout).unwrap());
     }
 
     #[test]
     fn return_val_0() {
-        return_val_num(0);
+        return_val_num("0");
     }
 
     #[test]
     fn return_val_123() {
-        return_val_num(123);
+        return_val_num("123");
     }
 }
