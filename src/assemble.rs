3a2c0f4f26dc8dc45cb170ec5216b4ea37a1cbc1
0yoyoyo <60439919+0yoyoyo@users.noreply.github.com>
2020-02-19 18:16:52 +0900

    Separate assemble functions to another file

---
+++ src/assemble.rs
@@ -0,0 +1,71 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+use NodeKind::*;
+use crate::parse::Node;
+use crate::parse::NodeKind;
+
+fn assemble_node(f: &mut File, node: Box<Node>) -> std::io::Result<()> {
+    match *node {
+        Node::Number {val} => {
+            f.write_fmt(format_args!("    push {}\n", val))?;
+        },
+        Node::Operator { kind, lhs, rhs} => {
+            assemble_node(f, lhs)?;
+            assemble_node(f, rhs)?;
+            f.write_fmt(format_args!("    pop rdi\n"))?;
+            f.write_fmt(format_args!("    pop rax\n"))?;
+            match kind {
+                NodeAdd => {
+                    f.write_fmt(format_args!("    add rax, rdi\n"))?;
+                },
+                NodeSub => {
+                    f.write_fmt(format_args!("    sub rax, rdi\n"))?;
+                },
+                NodeMul => {
+                    f.write_fmt(format_args!("    imul rax, rdi\n"))?;
+                },
+                NodeDiv => {
+                    f.write_fmt(format_args!("    cqo\n"))?;
+                    f.write_fmt(format_args!("    idiv rdi\n"))?;
+                },
+                NodeEq => {
+                    f.write_fmt(format_args!("    cmp rax, rdi\n"))?;
+                    f.write_fmt(format_args!("    sete al\n"))?;
+                    f.write_fmt(format_args!("    movzb rax, al\n"))?;
+                },
+                NodeNe => {
+                    f.write_fmt(format_args!("    cmp rax, rdi\n"))?;
+                    f.write_fmt(format_args!("    setne al\n"))?;
+                    f.write_fmt(format_args!("    movzb rax, al\n"))?;
+                },
+                NodeGr => {
+                    f.write_fmt(format_args!("    cmp rax, rdi\n"))?;
+                    f.write_fmt(format_args!("    setl al\n"))?;
+                    f.write_fmt(format_args!("    movzb rax, al\n"))?;
+                },
+                NodeGe => {
+                    f.write_fmt(format_args!("    cmp rax, rdi\n"))?;
+                    f.write_fmt(format_args!("    setle al\n"))?;
+                    f.write_fmt(format_args!("    movzb rax, al\n"))?;
+                },
+            }
+            f.write_fmt(format_args!("    push rax\n"))?;
+        },
+    }
+
+    Ok(())
+}
+
+pub fn assemble(f: &mut File, node: Box<Node>) -> std::io::Result<()> {
+    f.write_fmt(format_args!(".intel_syntax noprefix\n"))?;
+    f.write_fmt(format_args!(".global main\n"))?;
+    f.write_fmt(format_args!("main:\n"))?;
+
+    assemble_node(f, node)?;
+
+    f.write_fmt(format_args!("    pop rax\n"))?;
+    f.write_fmt(format_args!("    ret\n"))?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 mod token;
 mod parse;
+mod assemble;
 
 use std::env;
 use std::str;
 use std::fs;
 use std::fs::File;
-use std::io::prelude::*;
 
-use NodeKind::*;
 use token::tokenize;
 use token::Tokens;
-use parse::Node;
-use parse::NodeKind;
 use parse::expr;
+use assemble::assemble;
 
 fn make_output_dir() -> std::result::Result<(), String> {
     match fs::create_dir("output") {
@@ -25,71 +23,6 @@ fn make_output_dir() -> std::result::Result<(), String> {
             }
         },
     }
-}
-
-fn assemble_node(f: &mut File, node: Box<Node>) -> std::io::Result<()> {
-    match *node {
-        Node::Number {val} => {
-            f.write_fmt(format_args!("    push {}\n", val))?;
-        },
-        Node::Operator { kind, lhs, rhs} => {
-            assemble_node(f, lhs)?;
-            assemble_node(f, rhs)?;
-            f.write_fmt(format_args!("    pop rdi\n"))?;
-            f.write_fmt(format_args!("    pop rax\n"))?;
-            match kind {
-                NodeAdd => {
-                    f.write_fmt(format_args!("    add rax, rdi\n"))?;
-                },
-                NodeSub => {
-                    f.write_fmt(format_args!("    sub rax, rdi\n"))?;
-                },
-                NodeMul => {
-                    f.write_fmt(format_args!("    imul rax, rdi\n"))?;
-                },
-                NodeDiv => {
-                    f.write_fmt(format_args!("    cqo\n"))?;
-                    f.write_fmt(format_args!("    idiv rdi\n"))?;
-                },
-                NodeEq => {
-                    f.write_fmt(format_args!("    cmp rax, rdi\n"))?;
-                    f.write_fmt(format_args!("    sete al\n"))?;
-                    f.write_fmt(format_args!("    movzb rax, al\n"))?;
-                },
-                NodeNe => {
-                    f.write_fmt(format_args!("    cmp rax, rdi\n"))?;
-                    f.write_fmt(format_args!("    setne al\n"))?;
-                    f.write_fmt(format_args!("    movzb rax, al\n"))?;
-                },
-                NodeGr => {
-                    f.write_fmt(format_args!("    cmp rax, rdi\n"))?;
-                    f.write_fmt(format_args!("    setl al\n"))?;
-                    f.write_fmt(format_args!("    movzb rax, al\n"))?;
-                },
-                NodeGe => {
-                    f.write_fmt(format_args!("    cmp rax, rdi\n"))?;
-                    f.write_fmt(format_args!("    setle al\n"))?;
-                    f.write_fmt(format_args!("    movzb rax, al\n"))?;
-                },
-            }
-            f.write_fmt(format_args!("    push rax\n"))?;
-        },
-    }
-
-    Ok(())
-}
-
-fn assemble(f: &mut File, node: Box<Node>) -> std::io::Result<()> {
-    f.write_fmt(format_args!(".intel_syntax noprefix\n"))?;
-    f.write_fmt(format_args!(".global main\n"))?;
-    f.write_fmt(format_args!("main:\n"))?;
-
-    assemble_node(f, node)?;
-
-    f.write_fmt(format_args!("    pop rax\n"))?;
-    f.write_fmt(format_args!("    ret\n"))?;
-
-    Ok(())
 }
 
 fn generate_asm(formula: &str) -> std::result::Result<(), String> {
