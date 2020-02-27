363ae099875814824c2d1642fe54fcddb3d948a5
0yoyoyo <60439919+0yoyoyo@users.noreply.github.com>
2020-02-27 20:11:39 +0900

    Refactoring with "?"

--- src/parse.rs
+++ src/parse.rs
@@ -53,29 +53,21 @@ fn new_node_var(var: &str) -> Box<Node> {
 }
 
 fn primary(tokens: &mut Tokens) -> Result<Box<Node>, String> {
-    let node: Box<Node>;
     if tokens.expect_op("(") {
-        match expr(tokens) {
-            Ok(n) => node = n,
-            Err(e) => return Err(e),
-        }
-        if !tokens.expect_op(")") {
-            return Err(format!("Cannot find right parenthesis!"));
+        let node = expr(tokens)?;
+        if tokens.expect_op(")") {
+            Ok(node)
+        } else {
+            Err(format!("Cannot find right parenthesis!"))
         }
     } else if let Some(var) = tokens.expect_var() {
-        node = new_node_var(var);
+        Ok(new_node_var(var))
     } else {
-        let num: u32;
-        match tokens.expect_num() {
-            Some(n) => num = n,
-            None => {
-                print!("{}^ ", " ".repeat(tokens.head()));
-                return Err(format!("Not a number!"));
-            },
-        };
-        node = new_node_num(num);
+        let num = tokens.expect_num()
+                        .ok_or(format!("{}^ Not a number!",
+                            " ".repeat(tokens.head())))?;
+        Ok(new_node_num(num))
     }
-    Ok(node)
 }
 
 fn unary(tokens: &mut Tokens) -> Result<Box<Node>, String> {
@@ -88,22 +80,14 @@ fn unary(tokens: &mut Tokens) -> Result<Box<Node>, String> {
 }
 
 fn mul(tokens: &mut Tokens) -> Result<Box<Node>, String> {
-    let mut node: Box<Node>;
-    match unary(tokens) {
-        Ok(n) => node = n,
-        Err(e) => return Err(e),
-    }
+    let mut node = unary(tokens)?;
     while tokens.has_next() {
         if tokens.expect_op("*") {
-            match unary(tokens) {
-                Ok(rhs) => node = new_node(NodeMul, node, rhs),
-                Err(e) => return Err(e),
-            }
+            let rhs = unary(tokens)?;
+            node = new_node(NodeMul, node, rhs);
         } else if tokens.expect_op("/") {
-            match unary(tokens) {
-                Ok(rhs) => node = new_node(NodeDiv, node, rhs),
-                Err(e) => return Err(e),
-            }
+            let rhs = unary(tokens)?;
+            node = new_node(NodeDiv, node, rhs);
         } else {
             break;
         }
@@ -112,22 +96,14 @@ fn mul(tokens: &mut Tokens) -> Result<Box<Node>, String> {
 }
 
 fn add(tokens: &mut Tokens) -> Result<Box<Node>, String> {
-    let mut node: Box<Node>;
-    match mul(tokens) {
-        Ok(n) => node = n,
-        Err(e) => return Err(e),
-    }
+    let mut node = mul(tokens)?;
     while tokens.has_next() {
         if tokens.expect_op("+") {
-            match mul(tokens) {
-                Ok(rhs) => node = new_node(NodeAdd, node, rhs),
-                Err(e) => return Err(e),
-            }
+            let rhs = mul(tokens)?;
+            node = new_node(NodeAdd, node, rhs);
         } else if tokens.expect_op("-") {
-            match mul(tokens) {
-                Ok(rhs) => node = new_node(NodeSub, node, rhs),
-                Err(e) => return Err(e),
-            }
+            let rhs = mul(tokens)?;
+            node = new_node(NodeSub, node, rhs);
         } else {
             break;
         }
@@ -136,32 +112,20 @@ fn add(tokens: &mut Tokens) -> Result<Box<Node>, String> {
 }
 
 fn relational(tokens: &mut Tokens) -> Result<Box<Node>, String> {
-    let mut node: Box<Node>;
-    match add(tokens) {
-        Ok(n) => node = n,
-        Err(e) => return Err(e),
-    }
+    let mut node = add(tokens)?;
     while tokens.has_next() {
         if tokens.expect_op("<") {
-            match add(tokens) {
-                Ok(rhs) => node = new_node(NodeGr, node, rhs),
-                Err(e) => return Err(e),
-            }
+            let rhs = add(tokens)?;
+            node = new_node(NodeGr, node, rhs);
         } else if tokens.expect_op("<=") {
-            match add(tokens) {
-                Ok(rhs) => node = new_node(NodeGe, node, rhs),
-                Err(e) => return Err(e),
-            }
+            let rhs = add(tokens)?;
+            node = new_node(NodeGe, node, rhs);
         } else if tokens.expect_op(">") {
-            match add(tokens) {
-                Ok(lhs) => node = new_node(NodeGr, lhs, node),
-                Err(e) => return Err(e),
-            }
+            let lhs = add(tokens)?;
+            node = new_node(NodeGr, lhs, node);
         } else if tokens.expect_op(">=") {
-            match add(tokens) {
-                Ok(lhs) => node = new_node(NodeGe, lhs, node),
-                Err(e) => return Err(e),
-            }
+            let lhs = add(tokens)?;
+            node = new_node(NodeGe, lhs, node);
         } else {
             break;
         }
@@ -170,22 +134,14 @@ fn relational(tokens: &mut Tokens) -> Result<Box<Node>, String> {
 }
 
 fn equality(tokens: &mut Tokens) -> Result<Box<Node>, String> {
-    let mut node: Box<Node>;
-    match relational(tokens) {
-        Ok(n) => node = n,
-        Err(e) => return Err(e),
-    }
+    let mut node = relational(tokens)?;
     while tokens.has_next() {
         if tokens.expect_op("==") {
-            match relational(tokens) {
-                Ok(rhs) => node = new_node(NodeEq, node, rhs),
-                Err(e) => return Err(e),
-            }
+            let rhs = relational(tokens)?;
+            node = new_node(NodeEq, node, rhs);
         } else if tokens.expect_op("!=") {
-            match relational(tokens) {
-                Ok(rhs) => node = new_node(NodeNe, node, rhs),
-                Err(e) => return Err(e),
-            }
+            let rhs = relational(tokens)?;
+            node = new_node(NodeNe, node, rhs);
         } else {
             break;
         }
