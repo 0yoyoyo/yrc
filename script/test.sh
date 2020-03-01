59f0fed41f6da2015970fb721fe23922ceb419c2
0yoyoyo <60439919+0yoyoyo@users.noreply.github.com>
2020-03-01 18:35:46 +0900

    Change test script to set name

--- script/test.sh
+++ script/test.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+set -e
+
+if [ -n "$1" ]; then
+	NAME=$1
+else
+	NAME=tmp
+fi
+
 cd output
 
-gcc tmp.s -o tmp
-./tmp
-actual="$?"
+gcc $NAME.s -o $NAME
+./$NAME
+RESULT="$?"
 
-echo "$actual"
+echo "$RESULT"
