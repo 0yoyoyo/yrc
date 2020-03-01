5b368d24e9191f490c356c961c26aac09af40449
0yoyoyo <60439919+0yoyoyo@users.noreply.github.com>
2020-03-01 18:12:20 +0900

    Refactor test function

--- src/main.rs
+++ src/main.rs
@@ -118,74 +118,76 @@ mod tests {
     use super::*;
     use std::process::Command;
 
-    fn return_val_num(formula: &str, expect: u32) {
+    fn check_return_num(formula: &str, expect: u32) {
         compile(formula).unwrap();
-        let out = Command::new("bash")
-                          .arg("-c")
-                          .arg("script/test.sh")
-                          .output()
-                          .expect("Exec error!");
-        let answer = String::from_utf8(out.stdout).unwrap();
-        println!("{} -> {} (expected: {})",
-                 formula, answer, expect);
-        assert_eq!(format!("{}", expect), answer);
+        let output = Command::new("bash")
+            .arg("-c")
+            .arg("script/test.sh")
+            .output()
+            .unwrap();
+        let answer = str::from_utf8(&output.stdout)
+            .unwrap()
+            .parse()
+            .unwrap();
+        println!("{} -> {} (expected: {})", formula, answer, expect);
+        assert_eq!(expect, answer);
     }
 
     #[test]
     fn calc_unary() {
-        return_val_num("0;", 0);
-        return_val_num("123;", 123);
-        //return_val_num("-123;", -123);
-        return_val_num("(123);", 123);
-        //return_val_num("-(123);", -123);
+        check_return_num("0;", 0);
+        check_return_num("123;", 123);
+        //check_return_num("-123;", -123);
+        check_return_num("(123);", 123);
+        //check_return_num("-(123);", -123);
     }
 
     #[test]
     fn calc_binary() {
-        return_val_num("1 + 2;", 3);
-        return_val_num("3 - 2;", 1);
-        return_val_num("2 * 3;", 6);
-        return_val_num("6 / 2;", 3);
-        return_val_num("7 == 7;", 1);
-        return_val_num("7 == 8;", 0);
-        return_val_num("7 != 7;", 0);
-        return_val_num("7 != 8;", 1);
-        return_val_num("7 < 8;", 1);
-        return_val_num("7 <= 7;", 1);
-        return_val_num("7 <= 8;", 1);
-        return_val_num("7 < 7;", 0);
-        return_val_num("7 <= 6;", 0);
-        return_val_num("7 <= 6;", 0);
-        return_val_num("8 > 7;", 1);
-        return_val_num("7 >= 7;", 1);
-        return_val_num("8 >= 7;", 1);
-        return_val_num("7 > 7;", 0);
-        return_val_num("6 >= 7;", 0);
-        return_val_num("6 >= 7;", 0);
+        check_return_num("1 + 2;", 3);
+        check_return_num("3 - 2;", 1);
+        check_return_num("2 * 3;", 6);
+        check_return_num("6 / 2;", 3);
+        check_return_num("7 == 7;", 1);
+        check_return_num("7 == 8;", 0);
+        check_return_num("7 != 7;", 0);
+        check_return_num("7 != 8;", 1);
+        check_return_num("7 < 8;", 1);
+        check_return_num("7 <= 7;", 1);
+        check_return_num("7 <= 8;", 1);
+        check_return_num("7 < 7;", 0);
+        check_return_num("7 <= 6;", 0);
+        check_return_num("7 <= 6;", 0);
+        check_return_num("8 > 7;", 1);
+        check_return_num("7 >= 7;", 1);
+        check_return_num("8 >= 7;", 1);
+        check_return_num("7 > 7;", 0);
+        check_return_num("6 >= 7;", 0);
+        check_return_num("6 >= 7;", 0);
     }
 
     #[test]
     fn calc_combination() {
-        return_val_num("-1 + 2;", 1);
-        return_val_num("-(1 + 2) + 4;", 1);
-        return_val_num("2 * 3 + 6 / 2;", 9);
-        return_val_num("2 * (3 + 6) / 3;", 6);
+        check_return_num("-1 + 2;", 1);
+        check_return_num("-(1 + 2) + 4;", 1);
+        check_return_num("2 * 3 + 6 / 2;", 9);
+        check_return_num("2 * (3 + 6) / 3;", 6);
     }
 
     #[test]
     fn calc_local_variable() {
-        return_val_num("a = 1; a;", 1);
-        return_val_num("z = 1; z;", 1);
-        return_val_num("n = 10 + 2; n * 2;", 24);
+        check_return_num("a = 1; a;", 1);
+        check_return_num("z = 1; z;", 1);
+        check_return_num("n = 10 + 2; n * 2;", 24);
     }
 
     #[test]
     fn check_format() {
-        return_val_num("1+2+3;", 6);
-        return_val_num(" 1 + 2 + 3 ;", 6);
-        return_val_num("1 +  2   +    3;", 6);
-        return_val_num("(1+2)+3;", 6);
-        return_val_num("1+(2+3);", 6);
-        return_val_num("(1+2+3);", 6);
+        check_return_num("1+2+3;", 6);
+        check_return_num(" 1 + 2 + 3 ;", 6);
+        check_return_num("1 +  2   +    3;", 6);
+        check_return_num("(1+2)+3;", 6);
+        check_return_num("1+(2+3);", 6);
+        check_return_num("(1+2+3);", 6);
     }
 }
